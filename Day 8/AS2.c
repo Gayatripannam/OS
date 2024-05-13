f0c99de7fe1b66f8838af0e2338a84fca71b84f5
E1_GAYATRI_84190 <gayatripannam124@gmail.com>
2024-05-13 18:26:39 +0530

    eosAssignment

---
+++ Day 8/AS2.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<unistd.h>
+
+
+int main(){
+  int i, s1, s2, s3;
+  int ret1, ret2, ret3;
+  ret1 = fork();
+  if(ret1 == 0){
+      ret2 = fork();
+	  if(ret2 == 0){
+          ret3 = fork();
+		  if(ret3 == 0){
+            for(i=1; i<=5; i++) {
+			printf("child: %d\n", i);
+			sleep(1);
+			waitpid(ret3,&s3,0);
+			_exit(0);
+		  }
+
+            for(i=1; i<=5; i++) {
+			printf("child: %d\n", i);
+			sleep(1);
+			waitpid(ret2,&s2,0);
+			_exit(0);
+	  }
+
+            for(i=1; i<=5; i++) {
+			printf("child: %d\n", i);
+			sleep(1);
+			waitpid(ret1,&s1,0);
+			_exit(0);
+  }
+  for(i=1; i<=10; i++) {
+			printf("parent: %d\n", i);
+			sleep(1);
+  }
+          }
+      }
+ }
+return 0;
+}
+
+
---
+++ Day 8/AS3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+int main (){
+int ret,s,i,j;
+ i=1;
+while (1) {
+     ret=fork();
+	 if(ret ==0){
+	 printf("kranti");
+	 _exit(0);
+	}
+	else if( ret== -1)
+	   break;
+	else {
+	  i++;
+	  printf("child count : %d\n",i);
+	  }
+}
+
+for (i=1;j<=i;j++)
+     wait(&s);
+
+}	 
+
